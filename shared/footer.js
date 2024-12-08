1ac23edc94a37406b2ba762de6554bce57ede961
meakashkumar <meakashkumar.22@gmail.com>
2024-12-08 23:44:19 +0530

    akash change

---
+++ shared/footer.js
@@ -0,0 +1,82 @@
+document.getElementById("footer").innerHTML = `  <div class="container">
+            <div class="row row-cols-xxl-3 row-cols-xl-3 row-cols-lg-3 row-cols-md-2 row-cols-1 g-4">
+                <div class="col">
+                    <div class="about-footer">
+                        <img src="assets/img/white-logo.svg" alt="logo">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <ul>
+                            <li>
+                                <i class='bx bxs-map-pin text-white'></i>
+                                <p>Noida, Uttar Pradesh-201301</p>
+                            </li>
+                            <li>
+                                <a href="tel:8418092373"><i class='bx bxs-phone-call'></i> +91-8418092373</a>
+                            </li>
+                            <li>
+                                <a href="mailto:info@example.com"><i class='bx bxs-envelope'></i> info@example.com</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="footer-link">
+                        <ul>
+                            <li>
+                                <h5>Other Pages</h5>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Home</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> About Us</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Yoga History</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Yoga Teacher Training</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Yoga Activities</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Gallery</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="footer-link">
+                        <ul>
+                            <li>
+                                <h5>Quick Links</h5>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Terms & Conditions</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> Disclaimer</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class='bx bx-chevron-right'></i> FAQ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row row-cols-xxl-2 row-cols-xl-2 row-cols-lg-2 row-cols-md-2 row-cols-1 g-3">
+                <div class="col">
+                    <p>Diksha Yoga Shala</p>
+                </div>
+                <div class="col text-md-end">
+                    <p>Copyright © 2024 All rights reserved.</p>
+                </div>
+            </div>
+        </div>`;
---
+++ shared/header.js
@@ -0,0 +1,61 @@
+document.getElementById("header").innerHTML = `  <div class="container">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/img/logo.svg" alt="Logo">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    about
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Yoga history</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Yoga teacher training
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="yoga-activities.html">yoga activities</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">gallery</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Blogs</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact-us.html">contact us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Enroll now</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>`;
